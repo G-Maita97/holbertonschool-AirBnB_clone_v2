1f38077d14ebccc5c20f6516431639cbc9f39e33
G-Maita97 <gustavoalternativox@gmail.com>
2024-04-18 20:32:23 -0500

    Task 5 - create route with render_template from flask and work with HTML

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""Creacion de rutas con flask"""
+
+# Importamos clase Flask y la funcion render_template.
+from flask import Flask, render_template
+
+
+app = Flask(__name__)  # instanciamos la clase Flask.
+
+
+# Definir ruta con "strict_slashes=False"
+@app.route("/", strict_slashes=False)
+# funcion atada a la ruta
+def Hello_HBNB():
+    # Retorna codigo html
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    return "HBNB"
+
+
+# Definimos ruta con variables(texto)
+@app.route("/c/<text>", strict_slashes=False)
+def C_TEXT(text):
+    """
+    La función replace() es un método de cadena en Python
+    que se utiliza para reemplazar todas las ocurrencias de
+    un determinado substring (subcadena) dentro de una cadena
+    con otra subcadena.
+    """
+    text = text.replace('_', ' ')
+    """
+    la función devuelve una cadena que comienza con la
+    letra "C" seguida por el valor de la variable text
+    """
+    return f'C {text}'
+
+
+# Ruta /python/ tiene valor predeterminado para la variable text 'is_cool'.
+@app.route("/python/", strict_slashes=False)
+# definiendo funcion con variable
+@app.route("/python/<text>", strict_slashes=False)
+def python_text_value_default(text='is cool'):
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+
+# Create route with int variable.
+@app.route("/number/<int:n>", strict_slashes=False)
+def display_int(n):
+    return f'{n} is a number'
+
+
+# Crea ruta que muestra un archivo html con la funcion render_template.
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def display_HTML(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    # Configurar la aplicación para escuchar en 0.0.0.0 en el puerto 5000
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    """
+    debug=Tru para que Flask muestre mensajes
+    detallados de error en caso de que ocurra
+    un problema.
+    """
